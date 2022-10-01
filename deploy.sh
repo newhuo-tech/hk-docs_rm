7fed51219579d6f51af91866fd55056f20b2e8e2
wangziwen <114798800+newhuo-wangziwen@users.noreply.github.com>
2022-10-01 18:34:23 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -228,7 +228,7 @@ commit+push() {
   if [ $GH_TOKEN ]; then
     # deploy by Travis CI
     # add github token
-    repo="https://"$GH_TOKEN"@github.com/huobi-hk/docs.git"
+    repo="https://"$GH_TOKEN"@github.com/newhuo-tech/hk-docs"
     git remote add origin-pages $repo
     git push --quiet origin-pages $deploy_branch
   else
